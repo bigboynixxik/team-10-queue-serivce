fafacc7ab2d5e12b07297eca514d051382394098
dkmfzf <flim.win@yandex.ru>
2026-08-04 19:53:47 +0700

    feat(shared/model/zustand): added in model selectors

---
+++ frontend/src/shared/model/base/stateCreateReseter.ts
@@ -0,0 +1,21 @@
+import { create as actualCreate, type StateCreator } from "zustand";
+
+// https://zustand.docs.pmnd.rs/guides/how-to-reset-state
+
+export const storeResetFns = new Set<() => void>();
+
+export const resetAllStores = () => {
+	storeResetFns.forEach((resetFn) => {
+		resetFn();
+	});
+};
+
+export const create = (<T>() => {
+	return (stateCreator: StateCreator<T>) => {
+		const store = actualCreate(stateCreator);
+		storeResetFns.add(() => {
+			store.setState(store.getInitialState(), true);
+		});
+		return store;
+	};
+}) as typeof actualCreate;
