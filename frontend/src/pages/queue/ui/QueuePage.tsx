428879ae4b55efe0b02d7a5403cda7e4bc5ec6ae
dkmfzf <flim.win@yandex.ru>
2026-08-08 14:42:28 +0700

    feat: added pages slice

--- frontend/src/pages/queue/ui/QueuePage.tsx
+++ frontend/src/pages/queue/ui/QueuePage.tsx
@@ -1,7 +1,17 @@
+import { Alert } from '@ui';
+import { QueueSession } from '@widgets/queue-session';
+import { useParams } from 'react-router-dom';
+
 export function QueuePage() {
+  const { productId } = useParams();
+
   return (
-    <main>
-      <h1>Гойда 2</h1>
+    <main className="queue-page">
+      {productId ? (
+        <QueueSession productId={productId} />
+      ) : (
+        <Alert type="error" message="Товар не выбран" />
+      )}
     </main>
   );
 }
