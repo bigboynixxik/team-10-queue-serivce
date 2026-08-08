21688198f35b378c4275d394e3627d45573aa418
dkmfzf <flim.win@yandex.ru>
2026-08-08 14:36:44 +0700

    feat: added app slice

--- frontend/src/app/layout/AppLayout.tsx
+++ frontend/src/app/layout/AppLayout.tsx
@@ -1,5 +1,14 @@
 import { Outlet } from 'react-router-dom';
 
+import { useUserStore } from '@entities/user';
+import { Layout } from '@ui';
+
 export const AppLayout = (): React.JSX.Element => {
-  return <Outlet />;
-}
+  const userId = useUserStore.use.userId();
+
+  return (
+    <Layout className="app-shell" data-user-id={userId}>
+      <Outlet />
+    </Layout>
+  );
+};
