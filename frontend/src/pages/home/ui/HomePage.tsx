2dcce8a521950ce5054657572c3d8e49b3a1bafb
dkmfzf <flim.win@yandex.ru>
2026-08-08 14:42:28 +0700

    feat: added pages slice

--- frontend/src/pages/home/ui/HomePage.tsx
+++ frontend/src/pages/home/ui/HomePage.tsx
@@ -1,7 +1,17 @@
+import { Typography } from '@ui';
+import { ProductCatalog } from '@widgets/product-catalog';
+
 export function HomePage() {
   return (
-    <main>
-      <>Гойда</>
+    <main className="page-content">
+      <section className="hero">
+        <Typography.Title>Авито Очередь</Typography.Title>
+        <Typography.Paragraph>
+          Покупайте редкие товары честно: встаньте в очередь и получите время на оплату, когда товар
+          станет доступен.
+        </Typography.Paragraph>
+      </section>
+      <ProductCatalog />
     </main>
   );
 }
